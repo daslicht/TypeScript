ead76db231618d8cc91bfcf459f15485e5309631
Vladimir Matveev <vladima@microsoft.com>
2016-05-24 23:12:13 -0700

    add trivial check to avoid unnecessary scans

--- src/services/utilities.ts
+++ src/services/utilities.ts
@@ -267,6 +267,9 @@ namespace ts {
             // find the child that contains 'position'
             for (let i = 0, n = current.getChildCount(sourceFile); i < n; i++) {
                 let child = current.getChildAt(i);
+                if (position < child.getFullStart() || position > child.getEnd()) {
+                    continue;
+                }
                 let start = allowPositionInLeadingTrivia ? child.getFullStart() : child.getStart(sourceFile);
                 if (start <= position) {
                     let end = child.getEnd();
